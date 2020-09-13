7a0f391d62468188a5ce422fdb048d805705b9f5
EliasBautista <66222438+EliasBautista@users.noreply.github.com>
2020-09-13 13:47:04 -0500

    Initial commit

---
+++ app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
@@ -0,0 +1,54 @@
+package com.example.todoapp.fragments.list
+
+import android.os.Bundle
+import android.view.*
+import android.widget.LinearLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.todoapp.R
+import com.example.todoapp.data.viewmodel.ToDoViewModel
+import kotlinx.android.synthetic.main.fragment_list.view.*
+
+
+class ListFragment : Fragment() {
+
+    private val mToDoViewModel: ToDoViewModel by viewModels()
+
+    private val adapter: ListAdapter by lazy{ ListAdapter() }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Poblar el diseño para este fragmento
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        val recyclerView = view.recyclerView
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+
+        mToDoViewModel.getAllData.observe(viewLifecycleOwner, Observer { data ->
+            adapter.setData(data)
+        })
+
+        view.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        view.listLayout.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+        }
+
+        //set Menu
+        setHasOptionsMenu(true)
+
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
+    }
+}
